fd7f491959202b9b5d97d205437cff69c40e8958
Frank Tan <tansongyang@gmail.com>
2022-06-13 16:49:28 -0400

    Add lodash splitting
    Turns out this is free as long as we do
    
        import foo from 'lodash/foo';
    
    instead of
    
        import { foo } from 'lodash';

--- app/components/cart.js
+++ app/components/cart.js
@@ -1,6 +1,8 @@
 import Component from '@glimmer/component';
 import { items } from 'embroider-example/lib/cart';
 
+import reverse from 'lodash/reverse';
+
 export default class Cart extends Component {
-  items = items;
+  items = reverse(reverse(items));
 }
